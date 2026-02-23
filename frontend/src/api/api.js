543c0b0f05c07f75f0ce48e6c46a7b0ecaa42c2b
cheetah671 <arnavagnihotri507@gmail.com>
2026-02-24 01:11:42 +0530

    updated frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Since you are on Web, we simply use localhost
-const API_URL = 'http://localhost:5001/api'; 
+const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
+
 
 const api = axios.create({
     baseURL: API_URL,
