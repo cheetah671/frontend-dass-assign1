3b8c7a0d5bd7d95cd0c446c7c2673b98550ece5c
cheetah671 <arnavagnihotri507@gmail.com>
2026-02-24 00:40:05 +0530

    fix

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// const { defineConfig } = require('eslint/config');
+// const expoConfig = require('eslint-config-expo/flat');
+
+import { defineConfig } from 'eslint/config';
+import {expoConfig} from 'eslint-config-expo/flat';
+
 
 module.exports = defineConfig([
   expoConfig,
